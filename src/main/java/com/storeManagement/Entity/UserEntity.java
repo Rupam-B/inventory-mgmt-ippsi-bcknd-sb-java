e1997dd8e730fb894f8521b7269d34e9c92ad528
Rupam-b <rupam.banerje@gmail.com>
2024-10-16 17:04:42 +0530

    Identity generation type added

--- src/main/java/com/storeManagement/Entity/UserEntity.java
+++ src/main/java/com/storeManagement/Entity/UserEntity.java
@@ -19,7 +19,7 @@ import java.util.List;
 public class UserEntity {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId;
 
     private String userName;
