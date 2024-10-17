6ddfbb8d9135a990ac5f3959dfe9f21c2109d3d3
Rupam-b <rupam.banerje@gmail.com>
2024-10-17 11:46:04 +0530

    cors config class added

---
+++ src/main/java/com/storeManagement/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.storeManagement.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+   @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200", "https://inventory-mgmt-ipssi-rct.netlify.app", "http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
